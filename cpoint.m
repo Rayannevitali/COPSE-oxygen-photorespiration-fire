de2bf67a5c5c3aa369c8f0ec8500cf1b24899d8c
Rayanne Vitali <rvitali@envs.au.dk>
2025-08-31 15:00:48 +0200

    Update cpoint.m

--- cpoint.m
+++ cpoint.m
@@ -1,3 +1,7 @@
+%%%% CPOINT: Calculates the O2 compensation point for photosynthesis
+%%%%% Based on temperature (°C) and CO2 concentration (µmol/mol)
+%%%%% Following Haxeltine & Prentice (1996) and André (2011)
+
 function o2lim = cpoint(temp,co2)
 
 % set constant parameters 
@@ -7,4 +11,5 @@ sp   = 0.132;
 tau = sp*(q10t^((temp-25)/10)); % calculate rubisco specificity 
                                 % taken from haxeltine & prentice
 o2lim = (3/2)*co2*tau;          % O2 compensation point taken from andre (2011)
+
 end 
